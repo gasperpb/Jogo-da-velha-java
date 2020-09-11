f3b04df29bcc235b519488c6e032039e5a12a35d
gasperpb <achillespb@hotmail.com>
2020-09-10 21:28:10 -0300

    classe party

---
+++ Estudos/src/Estudos/Loops.java
@@ -0,0 +1,15 @@
+package Estudos;
+
+public class Loops {
+
+	public static void main(String[] args) {
+
+		int numero = 1;
+
+		while (numero < 6) {
+			System.out.println("Executou  "+numero);
+			numero++;
+		}
+
+	}
+}
